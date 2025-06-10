ab8ead996fdc1b4c9c24f2e3f76b7c8e576e385e
tofu <drewbryan08@gmail.com>
2025-06-11 10:10:22 +1200

    test web to make sure coolify resolves to the container properly

--- web/api/server.js
+++ web/api/server.js
@@ -1,1 +1,12 @@
-console.log("start");
+const expressapp = require("express")();
+expressapp.use(require("express").json());
+const { Pool } = require("pg");
+require("dotenv").config();
+
+const pool = new Pool({
+    url: process.env.DATABASE_URL,
+});
+
+expressapp.get("/api/ping", (req, res) => {
+    res.status(200).json({ message: "pong" });
+});

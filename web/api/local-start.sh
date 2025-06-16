8def50f17ace0b96dd961b188fc41d9bc2f14063
tofu <drewbryan08@gmail.com>
2025-06-16 14:51:00 +1200

    scores can be added and viewed

--- web/api/local-start.sh
+++ web/api/local-start.sh
@@ -3,7 +3,7 @@
 npm install
 
 echo "Waiting for db to be ready... (im in pain)"
-sleep 20
+sleep 5
 
 echo "Migrating now. If this fails, check db logs and restart this container."
 npx prisma migrate deploy
--- web/api/server.js
+++ web/api/server.js
@@ -1,16 +1,100 @@
+// Handles REST
 const expressapp = require("express")();
-expressapp.use(require("express").json());
-const { Pool } = require("pg");
+expressapp.use(require("express").json()); // Parses JSON bodies
+
+// Handles database queries
+const {PrismaClient} = require("@prisma/client");
+
+// Gets environment variables; set in docker or coolify
 require("dotenv").config();
 
-const pool = new Pool({
-    url: process.env.DATABASE_URL,
-});
+const pc = new PrismaClient();
 
+// Used to test if the server is reachable.
 expressapp.get("/ping", (req, res) => {
-    res.status(200).json({ message: "pong" });
+    res.status(200).send({message: "pong"});
 });
 
+expressapp.post("/addscore", async (req, res) => {
+
+    if (!req.body.name) {
+        return res.status(400).send({
+            status: 400,
+            error: "Name is required"
+        });
+    }
+
+    // Get the user with the name passed if it exists.
+    let user = await pc.user.findUnique({
+        where: {
+            name: req.body.name
+        }
+    }).catch((err) => {
+        return res.status(500).send({
+            status: 500,
+            error: err
+        });
+    });
+
+    // Create a new user if it does not exist.
+    if (!user) {
+        user = await pc.user.create({
+            data: {
+                name: req.body.name
+            }
+        }).catch((err) => {
+            return res.status(500).send({
+                status: 500,
+                error: err
+            });
+        });
+    }
+
+    // Create a new score record for the user with the score passed in the request body, and send an error on fail.
+    const scoreRecord = await pc.score.create({
+        data: {
+            userName: user["name"],
+            score: req.body.score
+        }
+    }).catch((err) => {
+        return res.status(500).send({
+            status: 500,
+            error: err
+        });
+    });
+
+    // Return the record created on success.
+    res.status(200).send({
+        status: 200,
+        score: scoreRecord
+    });
+});
+
+expressapp.get("/getleaderboard", async (req, res) => {
+    // Synchronously get all the scores from the database where the username contains the string in the search
+    // parameter, passed from the url (e.g. /getleaderboard?search=example). Send code 500 (internal error) if the query
+    // fails for some reason.
+    const scores = await pc.score.findMany({
+        where: {
+            userName: {
+                contains: req.query.search || "",
+                mode: "insensitive"
+            }
+        }
+    }).catch((err) => {
+        return res.status(500).send({
+            status: 500,
+            error: err
+        })
+    });
+
+    res.status(200).send({
+        status: 200,
+        scores: scores
+    });
+});
+
+// Start the server on port 3000, which is mapped as port 3000 on the host in the docker-compose and coolify.
 expressapp.listen(3000, () => {
     console.log("Server is running on port 3000");
 });
